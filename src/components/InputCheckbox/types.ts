98d067df5d8a6cd9489f2128f7ef557f0226e166
sovinbirla <123283605+sovinbirla@users.noreply.github.com>
2023-11-07 15:43:40 +0000

    Update 6 files

--- src/components/InputCheckbox/types.ts
+++ src/components/InputCheckbox/types.ts
@@ -3,8 +3,7 @@ import { FunctionComponent } from "react"
 type InputCheckboxProps = {
   id: string | number
   checked?: boolean
-  onChange: (newValue: boolean) => void
-  disabled?: boolean
+  onChange: (newValue: boolean) => boolean
 }
 
 export type InputCheckboxComponent = FunctionComponent<InputCheckboxProps>
